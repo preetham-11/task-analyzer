1dacc9c26982a17ee9548965c971ff2daeba3439
preetham-11 <preetursp@gmail.com>
2025-12-01 02:21:33 +0530

    Updated settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -56,8 +56,11 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "file://",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://smart-task-analyzer.onrender.com",
 ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
