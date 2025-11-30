be3f79263736988afd0ccfc3e8afa98c6480c7fa
preetham-11 <preetursp@gmail.com>
2025-12-01 03:11:15 +0530

    Preparing for deploy

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api/tasks';
+const API_BASE_URL = 'https://task-analyzer-2827.onrender.com/api/tasks';
 
 let tasksData = [];
 let completedTasks = new Set();
